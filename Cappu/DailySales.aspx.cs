043cb4365140d12eee37480525379aae284651e8
JazzSax <jhnbrnrdshgn@gmail.com>
2023-06-15 21:40:21 +0800

    fix bugs total error

--- Cappu/DailySales.aspx.cs
+++ Cappu/DailySales.aspx.cs
@@ -12,8 +12,10 @@ namespace Cappu
     {
 
         public string query;
+        string date;
         protected void Page_Load(object sender, EventArgs e)
         {
+            
             query = "SELECT CAST(SUM(total) AS DECIMAL(10, 2)) as TotalSum FROM orders INNER JOIN transactions ON orders.order_id = transactions.order_id WHERE order_date >= CONVERT(date, GETDATE())";
             ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
             information_show();
@@ -37,12 +39,12 @@ namespace Cappu
         }
         protected void search(object sender, EventArgs e)
         {
-          
 
+            date = DateTime.Parse(TextBox2.Text).ToString("MM-dd-yy") + " 23:59:59";
             using (Order order = new Order())
             {
 
-                string date = DateTime.Parse(TextBox2.Text).ToString("MM-dd-yy") + " 23:59:59";
+               
                 query = string.Format("SELECT t.order_id, o.order_date, t.product_name, t.quantity, CAST(t.total AS DECIMAL(10, 2)) as total " +
                        "FROM orders AS o " +
                        "INNER JOIN transactions AS t ON o.order_id = t.order_id " +
@@ -52,7 +54,7 @@ namespace Cappu
                 GridView2.DataBind();
 
             }
-            query = string.Format("SELECT  CAST(SUM(total) AS DECIMAL(10, 2)) as TotalSum  FROM orders INNER JOIN transactions ON orders.order_id = transactions.order_id WHERE order_date = '{0}'", TextBox2.Text);
+            query = string.Format("SELECT  CAST(SUM(total) AS DECIMAL(10, 2)) as TotalSum  FROM orders INNER JOIN transactions ON orders.order_id = transactions.order_id WHERE order_date >= '{0}' and order_date <= '{1}'", TextBox2.Text,date);
             compute_sum(query);
 
         }
